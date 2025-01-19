be6444c89e2e288cee3aeebb714ae231eb7277e6
Add2077 <supachai.rarkthong@gmail.com>
2025-01-19 22:54:49 +0700

    Delete dataconnect-generated/js/default-connector directory

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'free-nft',
-  location: 'us-central1'
-};
-
